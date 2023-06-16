67df3ee7d7131743f60a26edbeed632c6b1fc1a7
KALIDUWA OFC ✔️ <132440655+KALINDU-LK@users.noreply.github.com>
2023-06-16 20:10:30 +0530

    💚 SONIC-MD 💚

---
+++ plugins/main-Sonic.js
@@ -0,0 +1,21 @@
+let handler = async m => m.reply(`
+
+   *💝 SONIC-MD 💝* WA Group
+🔊 Join Public Bot Group And Support
+https://chat.whatsapp.com/LkP8QZqdhXUKxmc0hUPip5
+─────────────
+🔊 *Github*
+https://github.com/KALINDU-LK/SONIC-MD
+ 
+🔊 *Contact*
+wa.me:94758179948
+
+🔊 *Github*
+https://github.com/KALINDU-LK
+
+`.trim())
+handler.help = ['gpsonic']
+handler.tags = ['main']
+handler.command = ['groups', 'supportsonic', 'sonicp', 'groupsonic', 'group'] 
+
+export default handler
