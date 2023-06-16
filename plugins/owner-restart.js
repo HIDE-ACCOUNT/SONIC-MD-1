2f2868c997ec2641e51de951cc924c1284d48d59
KALIDUWA OFC ✔️ <132440655+KALINDU-LK@users.noreply.github.com>
2023-06-16 20:30:54 +0530

    💚 SONIC-MD 💚

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -2,14 +2,14 @@ import { spawn } from 'child_process'
 let handler = async (m, { conn, isROwner, text }) => {
     if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
     if (conn.user.jid == conn.user.jid) {
-    await m.reply('🇱🇰 Restarting *💝 Queen Hentai 💝* WA BOT...\n Plzz waiting..ohoma poddk idm besikayo🤣')
+    await m.reply('🇱🇰 Restarting *💝 SONIC-MD 💝* WA BOT...\n Plzz waiting..ohoma poddak idham besikayo🤣')
     process.send('reset')
   } else throw 'eh'
 }
 
 handler.help = ['restart']
 handler.tags = ['owner']
-handler.command = ['restart','hentairestart'] 
+handler.command = ['restart','sonicrestart'] 
 
 handler.rowner = true
 
