30ce8ab499b3779d8013d0a2ab5769750bbbfb8d
KALIDUWA OFC ✔️ <132440655+KALINDU-LK@users.noreply.github.com>
2023-06-16 15:20:57 +0530

    💚 SONIC-MD 💚

--- plugins/gp-link.js
+++ plugins/gp-link.js
@@ -2,7 +2,7 @@ import { areJidsSameUser } from '@adiwajshing/baileys'
 let handler = async (m, { conn, args }) => {
     let group = m.chat
     if (/^[0-9]{5,16}-?[0-9]+@g\.us$/.test(args[0])) group = args[0]
-    if (!/^[0-9]{5,16}-?[0-9]+@g\.us$/.test(group)) throw 'This Command Can only be used 💝 Queen Hentai 💝 Users in groups'
+    if (!/^[0-9]{5,16}-?[0-9]+@g\.us$/.test(group)) throw 'This Command Can only be used 💝 SONIC-MD 💝 Users in groups'
     let groupMetadata = await conn.groupMetadata(group)
     if (!groupMetadata) throw 'groupMetadata is undefined :\\'
     if (!('participants' in groupMetadata)) throw 'participants is not defined :('
