cec0c8798ff485e124c5eff2ec55745a7f03c666
KALIDUWA OFC ✔️ <132440655+KALINDU-LK@users.noreply.github.com>
2023-06-16 15:04:43 +0530

    💚 SONIC-MD 💚

--- plugins/audio-tomp3.js
+++ plugins/audio-tomp3.js
@@ -3,11 +3,11 @@ import { toAudio } from '../lib/converter.js'
 let handler = async (m, { conn, usedPrefix, command }) => {
     let q = m.quoted ? m.quoted : m
    /* let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
-    if (!/video|audio/.test(mime)) throw `🇱🇰 Mention to 💝 Queen Hentai 💝 a video or document for convert to mp3,with the command :\n\n*${usedPrefix + command}*`*/
+    if (!/video|audio/.test(mime)) throw `🇱🇰 Mention to 💝 SONIC-MD 💝 a video or document for convert to mp3,with the command :\n\n*${usedPrefix + command}*`*/
     let media = await q.download?.()
-    if (!media) throw '🇱🇰 Failed on 💝 Queen Hentai 💝 Server'
+    if (!media) throw '🇱🇰 Failed on 💝 SONIC-MD 💝 Server'
     let audio = await toAudio(media, 'mp4')
-    if (!audio.data) throw '🇱🇰 Error 💝 Queen Hentai 💝 Server'
+    if (!audio.data) throw '🇱🇰 Error 💝 SONIC-MD 💝 Server'
     conn.sendFile(m.chat, audio.data, 'audio.mp3', '', m, null, { mimetype: 'audio/mp4' })
 }
 handler.help = ['tomp3']
