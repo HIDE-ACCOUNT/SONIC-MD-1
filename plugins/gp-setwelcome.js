9e858c6da09c58b17b5d1c25b9e50d0de86d1432
KALIDUWA OFC ✔️ <132440655+KALINDU-LK@users.noreply.github.com>
2023-06-16 15:23:11 +0530

    💚 SONIC-MD 💚

--- plugins/gp-setwelcome.js
+++ plugins/gp-setwelcome.js
@@ -3,8 +3,8 @@
 let handler = async (m, { conn, text, isROwner, isOwner }) => {
   if (text) {
     global.db.data.chats[m.chat].sWelcome = text
-    m.reply('🇱🇰 The welcome message is configured by *💝 Queen Hentai 💝*')
-  } else throw `🔊 Enter the Welcome message to *💝 Queen Hentai 💝*\n\n@user (mention)\n@group (Group name)\n@desc (description of group)`
+    m.reply('🇱🇰 The welcome message is configured by *💝 SONIC-MD 💝*')
+  } else throw `🔊 Enter the Welcome message to *💝 SONIC-MD 💝*\n\n@user (mention)\n@group (Group name)\n@desc (description of group)`
 }
 handler.help = ['setwelcome <text>']
 handler.tags = ['group']
