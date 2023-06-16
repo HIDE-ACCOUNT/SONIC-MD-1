cf871dd02dfac8751beae2dd686b3c86bdb464a7
KALIDUWA OFC ✔️ <132440655+KALINDU-LK@users.noreply.github.com>
2023-06-16 15:15:49 +0530

    💚 SONIC-MD 💚

--- plugins/dl-yta.js
+++ plugins/dl-yta.js
@@ -1,8 +1,8 @@
 import { youtubedl, youtubedlv2, youtubedlv3 } from '@bochilteam/scraper'
 import fetch from 'node-fetch'
 let handler = async (m, { conn, args }) => {
-if (!args[0]) throw '*💝 Queen Hentai 💝 SEARCH YOUTUBE*'
-await m.reply(`*_💝 Processing Aᴜᴅɪᴏ BY 💝 Queen Hentai 💝..._*\n\n*💝  command .playdoc ᴏ .play.2 ᴏ .ytmp4doc 💝*`)
+if (!args[0]) throw '*💝 SONIC-MD 💝 SEARCH YOUTUBE*'
+await m.reply(`*_💝 Processing Aᴜᴅɪᴏ BY 💝 SONIC-MD 💝..._*\n\n*💝  command .playdoc ᴏ .play.2 ᴏ .ytmp4doc 💝*`)
 try {
 let q = '128kbps'
 let v = args[0]
@@ -18,7 +18,7 @@ let lolh = await lolhuman.json()
 let n = lolh.result.title || 'error'
 await conn.sendMessage(m.chat, { audio: { url: lolh.result.link }, fileName: `${n}.mp3`, mimetype: 'audio/mp4' }, { quoted: m })   
 } catch {
-await conn.reply(m.chat, '*💝 Queen Hentai 💝 AUDIO ERROR*', m)}
+await conn.reply(m.chat, '*💝 SONIC-MD 💝 AUDIO ERROR*', m)}
 }}
 handler.command = /^fgmp3|dlmp3|getaud|yt(a|mp3)$/i
 export default handler
