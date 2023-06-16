11cb9604eb2d6f9462829727188be0e19687272b
KALIDUWA OFC ✔️ <132440655+KALINDU-LK@users.noreply.github.com>
2023-06-16 20:21:56 +0530

    💚 SONIC-MD 💚

--- plugins/main-donate.js
+++ plugins/main-donate.js
@@ -3,8 +3,8 @@ let handler = async(m, { conn, usedPrefix, command }) => {
 
     let don = `
 🇱🇰 *DONATE* 🇱🇰
-If you get more info of 💝 Queen Hentai 💝 Please Contact 94740804536 💰`
-let img = 'https://i.imgur.com/bFEC7lC.jpeg'
+If you get more info of 💝 SONIC-MD 💝 Please Contact 94758179948 💰`
+let img = 'https://imgur.com/a/JtCzwRm'
 conn.sendFile(m.chat, img, 'img.jpg', don, m)
 }
 
