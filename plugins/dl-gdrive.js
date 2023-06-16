d3d4e3d9a297f952ffb43a88b6bcdeadd9a8ee27
KALIDUWA OFC ✔️ <132440655+KALINDU-LK@users.noreply.github.com>
2023-06-16 15:08:54 +0530

    💚 SONIC-MD 💚

--- plugins/dl-gdrive.js
+++ plugins/dl-gdrive.js
@@ -2,12 +2,12 @@
 import fg from 'api-dylux' 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 
-	if (!args[0]) throw `💝 Queen Hentai 💝, Enter a Google Drive link`
+	if (!args[0]) throw `💝 SONIC-MD 💝, Enter a Google Drive link`
 	m.react(rwait) 
 	try {
 	let res = await fg.GDriveDl(args[0])
 	 await m.reply(`
-*💝 Queen Hentai 💝 Google Drive DOWNLOADER*
+*💝 SONIC-MD 💝 Google Drive DOWNLOADER*
 
 💝 *Number:* ${res.fileName}
 💝 *Size:* ${res.fileSize}
